71d75d203bec4059efa5636cdb869b10b6be9b20
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2021-01-14 15:22:17 -0500

    Added view function with switch statement between cases

--- tracker.js
+++ tracker.js
@@ -18,7 +18,50 @@ connection.connect(function(err){
 
 //Function to view 
 
+function viewSearch {
+    inquirer
+    .prompt({
+        name: "view",
+        type: "rawlist",
+        message: "What would you like to view?",
+        choices: [
+            "View all employees",
+            "View all employees in a role",
+            "View an employee by ID",
+            "View all employees in a department",
+            "View a department"
+            
+        ]
+
+    })
+    .then(function(answer){
+        switch (answer.action) {
+            case "View all employees":
+                employeeSearch();
+                break;
+            
+            case "View all employees in a role":
+                roleSearch();
+                break;
+
+            case "View an employee by ID":
+                idSearch();
+                break;
+
+            case "View all employees in a department":
+                departmentSearch();
+                break;
+
+            case "View a department":
+                allDepartmentSearch();
+                break;
+        }
+    });
+}
+
 //Function to add 
+
+
 
 //Function to update 
 
